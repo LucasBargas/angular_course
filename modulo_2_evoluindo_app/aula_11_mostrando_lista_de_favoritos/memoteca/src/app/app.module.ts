2415ffe79fc1e8bdd26bc58d99ab5b5fbe4cd0d6
Lucas Bargas <lucasbargasoficial@gmail.com>
2025-04-20 21:08:46 -0300

    first commit

---
+++ modulo_2_evoluindo_app/aula_11_mostrando_lista_de_favoritos/memoteca/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CreateThinkComponent } from './components/thinks/create-think/create-think.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ListThinksComponent } from './components/thinks/list-thinks/list-thinks.component';
+import { ThinkComponent } from './components/thinks/think/think.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ExcludeThinkComponent } from './components/thinks/exclude-think/exclude-think.component';
+import { EditThinkComponent } from './components/thinks/edit-think/edit-think.component';
+import { RouterModule } from '@angular/router';
+import { ButtonLoadingMoreComponent } from './components/thinks/list-thinks/button-loading-more/button-loading-more.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    CreateThinkComponent,
+    ListThinksComponent,
+    ThinkComponent,
+    LoadingComponent,
+    ExcludeThinkComponent,
+    EditThinkComponent,
+    ButtonLoadingMoreComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
