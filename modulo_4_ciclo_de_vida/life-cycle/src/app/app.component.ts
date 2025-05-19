166ba1a0bcf4b00765380e9ae53ad096be1ea338
Lucas Bargas <lucasbargasoficial@gmail.com>
2025-05-19 16:51:30 -0300

    Aula 9 - completed

--- modulo_4_ciclo_de_vida/life-cycle/src/app/app.component.ts
+++ modulo_4_ciclo_de_vida/life-cycle/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { InputComponent } from "./components/input/input.component";
 import { Item } from './interfaces/iItem';
 import { ListaDeCompraService } from './service/lista-de-compra.service';
@@ -11,13 +11,18 @@ import { ItemComponent } from "./components/item/item.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, DoCheck {
   listaDeCompras!: Item[];
   itemParaSerEditado!: Item;
 
   constructor(
     private listaDeCompraService: ListaDeCompraService
   ) {}
+
+  ngDoCheck(): void {
+    console.log('DoCheck foi chamado')
+    this.listaDeCompraService.atualizarLocalStorage();
+  }
 
   ngOnInit(): void {
     this.listaDeCompras = this.listaDeCompraService.getListaDeCompra();
