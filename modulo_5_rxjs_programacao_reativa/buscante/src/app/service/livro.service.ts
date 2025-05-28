a6ce5ca6d3bfcb4959a23f960556a99fa72500cb
Lucas Bargas <lucasbargasoficial@gmail.com>
2025-05-27 22:05:05 -0300

    Aula 11 - completed

--- modulo_5_rxjs_programacao_reativa/buscante/src/app/service/livro.service.ts
+++ modulo_5_rxjs_programacao_reativa/buscante/src/app/service/livro.service.ts
@@ -13,12 +13,13 @@ export class LivroService {
     private http: HttpClient
   ) { }
 
-  buscar(valorDigitado: string): Observable<Item[]> {
+  buscar(valorDigitado: string): Observable<LivrosResultado> {
     const params = new HttpParams().append('q', valorDigitado);
-    return this.http.get<LivrosResultado>(`${this.API}`, { params }).pipe(
+    return this.http.get<LivrosResultado>(`${this.API}`, { params })
+    // .pipe(
       // tap(retornoAPI => console.log('Fluxo do TAP', retornoAPI)),
-      map(resultado => resultado.items),
+      // map(resultado => resultado.items ?? []),
       // tap(items => console.log('Fluxo após o MAP:', items))
-    );
+    // );
   }
 }
--- modulo_5_rxjs_programacao_reativa/buscante/src/app/views/lista-livros/lista-livros.component.ts
+++ modulo_5_rxjs_programacao_reativa/buscante/src/app/views/lista-livros/lista-livros.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 import { LivroComponent } from "../../componentes/livro/livro.component";
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { LivroService } from '../../service/livro.service';
-import { debounceTime, filter, map, switchMap, tap } from 'rxjs';
-import { Item } from '../../models/interfaces';
+import { catchError, debounceTime, EMPTY, filter, map, of, switchMap, tap, throwError } from 'rxjs';
+import { Item, LivrosResultado } from '../../models/interfaces';
 import { LivroVolumeInfo } from '../../models/LivroVolumeInfo';
 
 @Component({
@@ -16,16 +16,35 @@ import { LivroVolumeInfo } from '../../models/LivroVolumeInfo';
 })
 export class ListaLivrosComponent {
   campoBusca = new FormControl();
+  mensagemErro: string = '';
+  livrosResultado!: LivrosResultado;
 
   constructor(private livroService: LivroService) { }
 
-  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+  resultadoBusca$ = this.campoBusca.valueChanges.pipe(
     debounceTime(300), // Aguarda 300ms após o último evento de digitação
     filter((valorDigitado: string) => valorDigitado.length > 2),
     tap(() => console.log('Fluxo inicial')),
     switchMap((valorDigitado: string) => this.livroService.buscar(valorDigitado)),
+  )
+
+  totalDeLivros$ = this.resultadoBusca$.pipe(
+    map((resultado) => this.livrosResultado = resultado),
+    catchError((erro) => {
+      console.error(erro);
+      return of();
+    })
+  )
+
+  livrosEncontrados$ = this.resultadoBusca$.pipe(
     tap((retornoAPI) => console.log(retornoAPI)),
-    map((items: Item[]) => this.livrosResultadoParaLivros(items))
+    map(resultado => resultado.items ?? []),
+    map((items: Item[]) => this.livrosResultadoParaLivros(items)),
+    catchError((erro) => {
+      // this.mensagemErro = 'Erro ao buscar livros. Recarregue a aplicação ou tente novamente mais tarde.';
+      // return EMPTY;
+      return throwError(() => new Error(this.mensagemErro = 'Erro ao buscar livros. Recarregue a aplicação ou tente novamente mais tarde.'));
+    })
   )
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
