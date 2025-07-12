6f2f45aad247d91978e3147086d8b3cafc5a7aa9
Lucas Bargas <lucas.bargasoficial@gmail.com>
2025-07-12 00:37:33 -0300

    Jest configured

---
+++ tests/organo/jest.config.js
@@ -0,0 +1,21 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.js'], // Arquivo atualizado para .js
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|html)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.html$',
+      },
+    ],
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+    prefix: '<rootDir>/',
+  }),
+  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+};
